d135a4fb54aa2f2f0593f82b8f5c5760186a9eda
Bonafé <bonafe@opapagaiorinoceronte.net>
2023-09-02 14:51:21 -0300

    #6

--- src/python/metricas_qrlink.py
+++ src/python/metricas_qrlink.py
@@ -45,11 +45,15 @@ class MetricasQRLink:
     # Plota o gráfico        
     self.grafico_linha_dados = self.ax_dados.plot(self.dados['Tempo'], self.dados['Payload'], marker='o', linestyle='-')[0]
 
+
+
     self.fig_media_velocidade, self.ax_media_velocidade = plt.subplots(figsize=(8, 4))
     self.canvas_media_velocidade = FigureCanvas(self.fig_media_velocidade)
     self.ax_media_velocidade.set_title('Tempo vs. Mbps')
     self.ax_media_velocidade.set_xlabel('Tempo')
     self.ax_media_velocidade.set_ylabel('Mbps')    
+    # Plota o gráfico        
+    self.grafico_linha_media_velocidade = self.ax_media_velocidade.plot([], [], marker='o', linestyle='-')[0]
 
 
 
@@ -87,13 +91,14 @@ class MetricasQRLink:
 
 
   def imagem_grafico_medias(self):
+    
+    tempos_centrais, medias_mbps = self.gerar_lista_medias()  
 
-    return self.imagem_grafico_dados()    
+    self.grafico_linha_media_velocidade.set_xdata(tempos_centrais)
+    self.grafico_linha_media_velocidade.set_ydata(medias_mbps) 
 
-    self.dados_mbps = self.gerar_dataframe_medias()
-
-    # Plota o gráfico
-    self.ax_media_velocidade.plot(self.dados_mbps['Tempo'], self.dados_mbps['Média Mbps'], marker='o', linestyle='-')
+    self.grafico_linha_media_velocidade.axes.relim()
+    self.grafico_linha_media_velocidade.axes.autoscale_view()    
 
     # Atualiza o gráfico no pygame
     self.canvas_media_velocidade.draw()
@@ -113,44 +118,27 @@ class MetricasQRLink:
 
 
 
-  # Suponha que você já tenha o DataFrame 'dados' com as colunas 'Tempo', 'Payload' e 'QRCode Mode'
-  def gerar_dataframe_medias(self):
+  def gerar_lista_medias(self):
+        janela_tempo = 1  # 1 segundo
+        tempos_centrais = []
+        medias_mbps = []
 
+        inicio_janela = min(self.dados['Tempo'])
+        fim_janela = inicio_janela + janela_tempo
 
-        # Defina a janela de tempo desejada em segundos
-      janela_tempo = 1  # 1 segundo
+        while fim_janela <= max(self.dados['Tempo']):
+            dados_janela = [(tempo, payload) for tempo, payload in zip(self.dados['Tempo'], self.dados['Payload']) if inicio_janela <= tempo < fim_janela]
+            if dados_janela:
+                tempos, payloads = zip(*dados_janela)
 
-      # Inicialize listas para armazenar os tempos centrais e as médias do Mbps
-      tempos_centrais = []
-      medias_mbps = []
-    
+                tamanho_total = np.sum(payloads) * 8
+                tempo_total = fim_janela - inicio_janela
+                velocidade = tamanho_total / tempo_total
+                
+                tempo_central = (inicio_janela + fim_janela) / 2
+                tempos_centrais.append(tempo_central)
+                medias_mbps.append(velocidade)
+            inicio_janela = fim_janela
+            fim_janela += janela_tempo
 
-      # Itere sobre os dados para calcular as médias do Mbps em janelas de tempo
-      inicio_janela = self.dados['Tempo'].min()
-      fim_janela = inicio_janela + janela_tempo
-          
-      while fim_janela <= self.dados['Tempo'].max():
-          
-          # Filtre os dados dentro da janela de tempo
-          dados_janela = self.dados[(self.dados['Tempo'] >= inicio_janela) & (self.dados['Tempo'] < fim_janela)]
-          
-          # Calcule a média do Mbps para esta janela de tempo
-          media_mbps = np.mean((dados_janela['Payload'] * 8) / (dados_janela['Tempo'].diff() * 1000000))
-          
-          # Calcule o tempo central da janela
-          tempo_central = (inicio_janela + fim_janela) / 2                    
-
-          # Adicione os valores à lista
-          tempos_centrais.append(tempo_central)
-          medias_mbps.append(media_mbps)
-          
-          # Mova a janela para o próximo intervalo
-          inicio_janela = fim_janela
-          fim_janela += janela_tempo
-
-      # Crie um novo DataFrame com os tempos centrais e as médias do Mbps
-      novo_dataframe = pd.DataFrame({'Tempo': tempos_centrais, 'Média Mbps': medias_mbps})
-
-      # Exiba o novo DataFrame
-      return novo_dataframe
-  
+        return (tempos_centrais, medias_mbps)
